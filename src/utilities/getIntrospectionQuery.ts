02538a5efc33277254c60817954881092717156c
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2020-11-18 10:24:12 -0600

    remove `<>` (flow) from unions

--- src/utilities/getIntrospectionQuery.ts
+++ src/utilities/getIntrospectionQuery.ts
@@ -244,10 +244,10 @@ export type IntrospectionNonNullTypeRef<
 };
 
 export type IntrospectionTypeRef =
-  | IntrospectionNamedTypeRef<>
-  | IntrospectionListTypeRef<>
+  | IntrospectionNamedTypeRef
+  | IntrospectionListTypeRef
   | IntrospectionNonNullTypeRef<
-      IntrospectionNamedTypeRef<> | IntrospectionListTypeRef<>
+      IntrospectionNamedTypeRef | IntrospectionListTypeRef
     >;
 
 export type IntrospectionOutputTypeRef =
